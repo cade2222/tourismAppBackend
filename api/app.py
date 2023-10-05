1263c6fe4bd10d04cbc652cdabef0eb8f35f04ea
sam-cp <samprewett@hotmail.com>
2023-10-05 17:12:27 -0500

    Added DB connectivity

--- api/app.py
+++ api/app.py
@@ -1,4 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+from contextlib import contextmanager
+import psycopg2
+import psycopg2.extras
+
+@contextmanager
+def dbcursor():
+    filename = request.environ.get("PSQL_CONF")
+    if filename is None:
+        raise RuntimeError("Environment variable PSQL_CONF is not set.")
+    connstr = None
+    try:
+        file = open(filename)
+        connstr = file.read()
+    except OSError:
+        raise RuntimeError("Could not open PSQL_CONF.")
+    finally:
+        file.close()
+    
+    try:
+        conn = psycopg2.connect(connstr)
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        yield cur
+    except psycopg2.Error as e:
+        raise RuntimeError(e.pgerror)
+    finally:
+        cur.close()
+        conn.close()
+
 
 app = application = Flask(__name__)
 
@@ -10,4 +38,9 @@ def hello():
 
 @app.route('/apicall')
 def apicall():
-    return {"user" : "David"}
+    users = []
+    with dbcursor() as cur:
+        cur.execute("SELECT username, displayname, email FROM Users;")
+        for row in cur:
+            users.append(dict(row))
+    return users
