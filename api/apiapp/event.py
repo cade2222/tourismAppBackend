c37d1a59fd87a8ef40c116d6de875710891f6a71
sam-cp <samprewett@hotmail.com>
2023-10-18 12:55:37 -0500

    Add latitude and longitude to database

--- api/apiapp/event.py
+++ api/apiapp/event.py
@@ -4,29 +4,35 @@ from .auth import authenticate
 
 bp = Blueprint("event", __name__, url_prefix="/event")
 
-def validate_create_inputs(displayname: str, description: str | None = None, **kwargs) -> list:
+def validate_create_inputs(displayname: str, description: str | None = None, location: dict | None = None, **kwargs) -> list:
     """
     Ensures the given event information is valid.
 
     Returns a list of errors (see docstring for `create()`), or an empty list if no issues were found.
     """
+    errors = []
     if not displayname:
-        return [{"field": "displayname", "description": "You must enter a display name."}]
+        errors.append({"field": "displayname", "description": "You must enter a display name."})
     if len(displayname) > 255:
-        return [{"field": "displayname", "description": "Display name must be less than 256 characters long."}]
+        errors.append({"field": "displayname", "description": "Display name must be less than 256 characters long."})
     if description is not None and len(description) > 10000:
-        return [{"field": "description", "description": "Description must be at most 10000 characters long."}]
-    return []
+        errors.append({"field": "description", "description": "Description must be at most 10000 characters long."})
+    if not -90.0 <= float(location["lat"]) <= 90.0:
+        errors.append({"field": "location", "description": "Latitude must be between -90 and 90 degrees."})
+    return errors
 
-def create_event(displayname: str, description: str | None = None, **kwargs) -> None:
+def create_event(displayname: str, description: str | None = None, location: dict | None = None, **kwargs) -> None:
     """
     Adds an event with the given information to the database.
     """
     assert isinstance(g.conn, psycopg.Connection)
     assert isinstance(g.userid, int)
     with g.conn.cursor() as cur:
-        cur.execute("INSERT INTO events(displayname, description, host) VALUES (%s, %s, %s);", 
+        cur.execute("INSERT INTO events(displayname, description, host) VALUES (%s, %s, %s) RETURNING id;", 
                     (displayname, description if description is not None else "", g.userid))
+        if location is not None:
+            id, = cur.fetchone()
+            cur.execute("UPDATE events SET latitude = %s, longitude = %s WHERE id = %s;", (location["lat"], location["lon"], id))
 
 @bp.route("/create", methods=["POST"])
 @authenticate
@@ -37,11 +43,15 @@ def create():
     Requires a JSON object in the request body with the following properties:
         - `displayname`: the name of the event
         - `description` (optional): a description of the event
+        - `location` (optional): a JSON object containing the coordinates of the event
+            - `lat`: the latitude, in degrees
+            - `lon`: the longitude, in degrees
     
     The input data must meet the following requirements:
         - `displayname` must not be empty.
         - `displayname` must contain no more than 255 characters.
         - `description` must contain no more than 10000 characters.
+        - `location.lat` must be between -90 and 90, inclusive
     
     The following status codes will be returned:
         - 415 if the request is not in JSON format.
@@ -50,7 +60,7 @@ def create():
         - 200 if the JSON is semantically correct, even if other errors are returned.
     
     If a 200 status code is returned, the response body will contain a JSON array of error objects containing the following properties:
-        - `field`: the field in which the error occurred
+        - `field`: the top-level field in which the error occurred
         - `description`: a description of the error
     
     If this array is non-empty, the request was not successful.
@@ -64,6 +74,16 @@ def create():
         abort(422)
     if "description" in request.json and not isinstance(request.json["description"], str):
         abort(422)
+    if "location" in request.json:
+        if not isinstance(request.json["location"], dict):
+            abort(422)
+        if "lat" not in request.json["location"] or "lon" not in request.json["location"]:
+            abort(422)
+        try:
+            _ = float(request.json["location"]["lat"])
+            _ = float(request.json["location"]["lon"])
+        except ValueError:
+            abort(422)
     errors = validate_create_inputs(**request.json)
     if not errors:
         create_event(**request.json)
