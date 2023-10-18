6cbcafbca797a0303ff61696e7fdfb7145c077cb
sam-cp <samprewett@hotmail.com>
2023-10-18 12:40:43 -0500

    Add docstrings and restrict event creation method

--- api/apiapp/event.py
+++ api/apiapp/event.py
@@ -5,6 +5,11 @@ from .auth import authenticate
 bp = Blueprint("event", __name__, url_prefix="/event")
 
 def validate_create_inputs(displayname: str, description: str | None = None, **kwargs) -> list:
+    """
+    Ensures the given event information is valid.
+
+    Returns a list of errors (see docstring for `create()`), or an empty list if no issues were found.
+    """
     if not displayname:
         return [{"field": "displayname", "description": "You must enter a display name."}]
     if len(displayname) > 255:
@@ -14,15 +19,43 @@ def validate_create_inputs(displayname: str, description: str | None = None, **k
     return []
 
 def create_event(displayname: str, description: str | None = None, **kwargs) -> None:
+    """
+    Adds an event with the given information to the database.
+    """
     assert isinstance(g.conn, psycopg.Connection)
     assert isinstance(g.userid, int)
     with g.conn.cursor() as cur:
         cur.execute("INSERT INTO events(displayname, description, host) VALUES (%s, %s, %s);", 
                     (displayname, description if description is not None else "", g.userid))
 
-@bp.route("/create")
+@bp.route("/create", methods=["POST"])
 @authenticate
 def create():
+    """
+    API endpoint for creating a new event.
+
+    Requires a JSON object in the request body with the following properties:
+        - `displayname`: the name of the event
+        - `description` (optional): a description of the event
+    
+    The input data must meet the following requirements:
+        - `displayname` must not be empty.
+        - `displayname` must contain no more than 255 characters.
+        - `description` must contain no more than 10000 characters.
+    
+    The following status codes will be returned:
+        - 415 if the request is not in JSON format.
+        - 400 if the JSON is malformatted.
+        - 422 if the JSON is not of the right type or does not contain the right values.
+        - 200 if the JSON is semantically correct, even if other errors are returned.
+    
+    If a 200 status code is returned, the response body will contain a JSON array of error objects containing the following properties:
+        - `field`: the field in which the error occurred
+        - `description`: a description of the error
+    
+    If this array is non-empty, the request was not successful.
+    If the array is empty, the request can be assumed to have been successful.
+    """
     if request.json is None:
         abort(415)
     if not isinstance(request.json, dict):
