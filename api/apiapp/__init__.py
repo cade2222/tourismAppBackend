1a912bb328fa718285550e176687c4ac38e109f8
sam-cp <samprewett@hotmail.com>
2023-11-25 14:44:52 -0600

    Added CORS

--- api/apiapp/__init__.py
+++ api/apiapp/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app() -> Flask:
     """
     Creates an application instance (for use by mod_wsgi)
     """
     app = Flask(__name__)
+    CORS(app)
 
     from . import auth, db, event, location, research
     app.register_blueprint(auth.bp)
